71b5a3a3087dad457d1e462596166fae3717a859
safdar razwan <safdar.razwan@nfs.school>
2025-11-14 15:38:38 +0100

    add form .php add connexion.php

---
+++ include/connexion.php
@@ -0,0 +1,20 @@
+<?php
+include 'include/header.php';
+?>
+
+<h2>Page de connexion</h2>
+<section id="formConnexion">
+
+    <form action="#" method="POST">
+        <input type="email" id="email" placeholder="userEmail" require>
+        <input type="password" placeholder="userPassword">
+        <button type="submit">Envoyer</button>
+
+</section>
+</body>
+</html>
+
+<?php
+
+include 'include/footer.php';
+?>
---
+++ include/form.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>formulaire</title>
+</head>
+<body>
+<header></header>
+<section formulaire=" ">
+    <input type="text" id=" nom" name="nom" required>
+    <input type="email" id="email" name="email" require>
+    <input type="password" id="mot de passe">
+
+</section>
+</body>
+</html>
